89fd03d5206ae4b36e93d3e252741803992c98bc
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-09-19 17:39:15 +0200

    Initial commit

---
+++ src/main/java/com/olejarzikt/springboot/thymeleaftest/ThymeleaftestApplication.java
@@ -0,0 +1,13 @@
+package com.olejarzikt.springboot.thymeleaftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleaftestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleaftestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olejarzikt/springboot/thymeleaftest/ThymeleaftestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olejarzikt.springboot.thymeleaftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleaftestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
