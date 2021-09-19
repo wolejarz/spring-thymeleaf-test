f3a597366ffc20a640c4dda2f24c11ab8f696439
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2021-09-19 17:53:33 +0200

    Controller modified

--- src/main/java/com/olejarzikt/springboot/thymeleaftest/controller/ThymeleafController.java
+++ src/main/java/com/olejarzikt/springboot/thymeleaftest/controller/ThymeleafController.java
@@ -1,2 +1,15 @@
-package com.olejarzikt.springboot.thymeleaftest.controller;public class ThymeleafController {
+package com.olejarzikt.springboot.thymeleaftest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+
+    @GetMapping("/thymeleaf")
+    public String sayHello(Model theModel) {
+        theModel.addAttribute("theData",new java.util.Date());
+        return "helloworld";
+    }
 }
